457473f7ec2f6014c42e7593392e8d20a85cf690
Łukasz Szczygielski <lukasz.szczygielski@britenet.com.pl>
2019-10-10 19:34:21 +0200

    Configure server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/trenzone-front'));
+app.use(express.static(__dirname + '/dist/trenzone-html5));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/trenzone-front/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/trenzone-html5/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
