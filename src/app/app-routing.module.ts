2ba09a8d4c59766b8003a473bf52674882651439
Łukasz Szczygielski <lukasz.szczygielski@britenet.com.pl>
2019-10-12 20:39:06 +0200

    Clear unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {TrainingListComponent} from './training-list/training-list.component';
-import {TrainingFormComponent} from './training-form/training-form.component';
-import {TrainingCreatorComponent} from './training-creator/training-creator.component';
 import {ExercisesListComponent} from './exercises-list/exercises-list.component';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './auth/components/login/login.component';
 import {RegisterComponent} from './auth/components/register/register.component';
-import {NewTrainingComponent} from './pages/new-training/new-training.component';
-import {TrainigEditionComponent} from './pages/trainig-edition/trainig-edition.component';
 import {TrainingComponent} from './pages/training/training.component';
 
 const routes: Routes = [
   {path: 'training', component: TrainingListComponent},
-  {path: 'addtraining', component: TrainingFormComponent},
-  {path: 'newaddtraining', component: TrainingCreatorComponent},
   {path: 'exercise', component: ExercisesListComponent},
   {path: 'home', component: HomeComponent},
   {path: 'auth/login', component: LoginComponent},
